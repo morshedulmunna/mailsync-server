88be3cab0012a39403275ba7f14a2f5a33735586
morshedulmunna <morshedulmunna1@gmail.com>
2023-07-23 19:01:22 +0600

    git cache cleared

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { EmailService } from './email.service';
 
@@ -15,27 +7,27 @@ export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
   @Post('/sent')
-  create(@Body() createEmailDto: Prisma.EmailCreateInput) {
-    return this.emailService.create(createEmailDto);
+  emailSending(@Body() createEmailDto: Prisma.EmailCreateInput) {
+    return this.emailService.emailSending(createEmailDto);
   }
 
-  @Get()
-  findAll() {
-    return this.emailService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.emailService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.emailService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.emailService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmailDto: any) {
-    return this.emailService.update(+id, updateEmailDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateEmailDto: any) {
+  //   return this.emailService.update(+id, updateEmailDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.emailService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.emailService.remove(+id);
+  // }
 }

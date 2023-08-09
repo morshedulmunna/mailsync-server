6e54e53cc415a84e1379ca259d2c0883f7928ff8
morshedulmunna <morshedulmunna1@gmail.com>
2023-08-09 20:42:48 +0600

    corse enable

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET, PUT, POST, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   app.setGlobalPrefix('/api/v1');
   await app.listen(5000);
 }

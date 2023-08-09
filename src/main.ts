48ebd04d5a29177383c63b4f42d03a206c40bf2c
morshedulmunna <morshedulmunna1@gmail.com>
2023-08-09 20:05:16 +0600

    schema change

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   app.setGlobalPrefix('/api/v1');
   await app.listen(5000);
 }

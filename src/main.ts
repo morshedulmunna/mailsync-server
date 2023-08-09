279fcfce0c5909a980efdc0fafcdf16c77da6785
morshedulmunna <morshedulmunna1@gmail.com>
2023-08-09 20:51:44 +0600

    cors policy change

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Change this to the specific origin you want to allow
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.setGlobalPrefix('/api/v1');
   await app.listen(5000);
 }

ef84c29d94f1b90e14a07743c50f7845a7162874
morshedulmunna <morshedulmunna1@gmail.com>
2023-08-10 14:22:02 +0600

    cors issue

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   // Enable CORS for specific origins
   app.enableCors({
-    origin: ['https://mailsyncs.vercel.app/', 'http://localhost:3000/'],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Enable sending cookies and other credentials
   });
   app.setGlobalPrefix('/api/v1');
   await app.listen(5000);
